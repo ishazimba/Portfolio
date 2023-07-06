09b0df48a70533ab0e76069b51795c416b5f4cb9
ishazimba <isha.zimba@gmail.com>
2023-07-07 00:30:17 +0100

    images added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,10 +7,10 @@ const Projects = () => {
       <h1>Portfolio</h1>
       <div className="projects-container">
         <div className="project-container">
-          <Link to="/smwa" className="project-link">
+          <Link to="/fp" className="project-link">
             <img
-              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/SMWA/SMWA.png"
-              alt="Student mental wellbeing application"
+              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/fp/fp%20-front.png"
+              alt="FP CAPITAL"
             />
             <h3>Student Mental Wellbeing Application</h3>
             <p>
@@ -41,17 +41,15 @@ const Projects = () => {
             <h3>Student Mental Wellbeing Application</h3>
             <p>
               Designing the user experience of a student mental wellbeing
-              application that is intended to be used by students seeking mental
-              well-being tips, suggestions, and solutions to cope with their
-              situation application.
+              application.
             </p>
           </Link>
         </div>
         <div className="project-container">
           <Link to="/fp" target="_blank">
             <img
-              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/SMWA/SMWA.png"
-              alt="Student mental wellbeing application"
+              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/fp/fp%20-front.png"
+              alt="FP CAPITAL"
             />
           </Link>
         </div>
