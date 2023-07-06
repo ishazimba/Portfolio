9fb654fdad05472e52a50f7b15dadbfaccd0784b
ishazimba <isha.zimba@gmail.com>
2023-07-07 00:55:52 +0100

    more images added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,24 +9,57 @@ const Projects = () => {
         <div className="project-container">
           <Link to="/fp" className="project-link">
             <img
-              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/fp/fp%20-front.png"
+              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/nc%20news/nc-api.png"
               alt="FP CAPITAL"
             />
-            <h3>Student Mental Wellbeing Application</h3>
+            <h3>NC News API</h3>
             <p>
-              Designing the user experience of a student mental wellbeing
-              application that is intended to be used by students seeking mental
-              well-being tips, suggestions, and solutions to cope with their
-              situation application.
+              A RESTful API designed to interact with a news database containing
+              articles, topics, users, and comments.
             </p>
           </Link>
         </div>
         <div className="project-container">
           <Link to="/fp" target="_blank">
             <img
+              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/nc%20news/nc-front-end.png"
+              alt="Student mental wellbeing application"
+            />
+            <h3>NC News</h3>
+            <p>
+              A React application that provides users with a platform to view
+              and interact with articles sourced from the NC News RESTful API.
+            </p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="projects-container">
+        <div className="project-container">
+          <Link to="/fp" target="_blank">
+            <img
+              src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/airpark/airpark.png"
+              alt="airpark"
+            />
+            <h3>AirPark</h3>
+            <p>
+              A mobile app that offers a seamless and convenient parking
+              experience for the travelers.
+            </p>
+          </Link>
+        </div>
+
+        <div className="project-container">
+          <Link to="/smwa" className="project-link">
+            <img
               src="https://raw.githubusercontent.com/ishazimba/Portfolio/main/images/SMWA/SMWA.png"
               alt="Student mental wellbeing application"
             />
+            <h3>Student Mental Wellbeing Application</h3>
+            <p>
+              Designing the user experience of a student mental wellbeing
+              application.
+            </p>
           </Link>
         </div>
       </div>
