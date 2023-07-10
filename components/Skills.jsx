eb897d34219c151eb51b3380c37e35374642962c
ishazimba <isha.zimba@gmail.com>
2023-07-10 19:33:57 +0100

    slider changed

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -21,8 +21,8 @@ const Skills = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToShow: 6,
+    slidesToScroll: 5,
     autoplay: true, // Enable autoplay
     autoplaySpeed: 1500, // Set autoplay speed in milliseconds
     nextArrow: <CustomNextArrow />,
@@ -31,19 +31,19 @@ const Skills = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 5,
         },
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
         },
       },
     ],
